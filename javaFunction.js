600a545f06a4fac52954942f74a6b93fb439848a
Muhammad Shahid Rafique <64875260+shahid-rafique@users.noreply.github.com>
2020-06-09 23:02:15 -0400

    Add files via upload

---
+++ javaFunction.js
@@ -0,0 +1,9 @@
+	// Start function
+	function multiply(num1, num2){
+	//return num2; // function execution ends here
+	return num1 * num2;
+	}
+	var a = multiply(3,6); // returns 6
+	alert(a);
+	console.log(a); // 6
+	// end function
